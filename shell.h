db2ddd1a7badfb8830685aab060802951d108a69
Danielle Badobre <badobredanielle@gmail.com>
2023-10-16 13:58:14 +0000

    deleted all the strtok files

--- shell.h
+++
@@ -1,7 +0,0 @@
-#ifndef S_HELL
-#define S_HELL
-unsigned int custom_strspn(char *str, char *findem);
-unsigned int custom_strcspn(char *str, char *notyet);
-char *custom_strtok(char *str, char *delim, char **ptr_saved);
-char *custom_strtokenizer(char *str, char *delim);
-#endif
